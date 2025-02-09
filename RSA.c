309ea1d2d968a678a5d3970d5a59beaef40241b0
TeithiUnigol <teithiunigol@gmail.com>
2025-02-09 16:58:03 +0100

    almost

--- RSA.c
+++ RSA.c
@@ -3,15 +3,28 @@
 
 unsigned long long modularisHatvanyozas(unsigned long long a,unsigned long long b, unsigned long long m);
 char* Coder(char* original,int code,unsigned long long n);
+char* Decoder(char* coded, int code, unsigned long long n, unsigned long long fi);
+int lnko(int num1,int num2);
 
 unsigned long long EulerFi(unsigned long long prime1,unsigned long long prime2){
     return (prime1-1)*(prime2-1);
 }
 
+unsigned long long modInverse(unsigned long long a, unsigned long long m) ;
+unsigned long long extendedGCD(unsigned long long a, unsigned long long b, unsigned long long *x, unsigned long long *y);
+
+void numprinter(char* szoveg){
+    for (int i = 0;szoveg[i]!='\0'; ++i)
+    {
+        printf("%d-",(int)szoveg[i]);
+    }
+    printf("\n");
+}
+
 int main(){
     unsigned long long prime1 = 4294991927;
     unsigned long long prime2 = 4294997149;
-    unsigned long long prime3 = 4294991887;
+    /*unsigned long long prime3 = 4294991887;
     unsigned long long prime4 = 4294983661;
     unsigned long long prime5 = 4294995121;
 
@@ -19,14 +32,23 @@ int main(){
     int prime7 = 0;
     int prime8 = 0;
     int prime9 = 0;
-    int prime10 = 0;
-    char* coded = Coder("alma",10,EulerFi(prime1,prime2));
-    for (int i = 0;coded[i]!='\0'; ++i)
-    {
-        printf("%c -- %d\n",coded[i],(int)coded[i]);
-    }
+    int prime10 = 0;*/
+
+    int code = 11;
+    char* original = "alma";
+    char* coded = Coder(original,code,prime1*prime2);
     
+    printf("Eredeti:");
+    numprinter(original);
+    printf("Coded:");
+    numprinter(coded);
+    
+    unsigned long long fi = EulerFi(prime1, prime2);
+    char* decoded = Decoder(coded, code, prime1 * prime2, fi);
+    printf("Vissza: %s\n", decoded);
+
     free(coded);
+    free(decoded);
 }
 
 char* Coder(char* original,int code,unsigned long long n){
@@ -46,6 +68,23 @@ char* Coder(char* original,int code,unsigned long long n){
    return coded;
 }
 
+char* Decoder(char* coded, int code, unsigned long long n, unsigned long long fi) {
+    int len = 0;
+    while (coded[len] != '\0') {
+        ++len;
+    }
+
+    // Kiszámítjuk a privát kulcsot (d), ami az inverz
+    unsigned long long d = modInverse(code, fi);
+
+    char* decoded = malloc(sizeof(char) * (len + 1));
+    for (int i = 0; i < len; ++i) {
+        decoded[i] = (char)(modularisHatvanyozas((int)coded[i], d, n));
+    }
+    decoded[len] = '\0';
+    return decoded;
+}
+
 unsigned long long modularisHatvanyozas(unsigned long long a,unsigned long long b, unsigned long long m) {//a^b(mod m)
     unsigned long long na = a;
     unsigned long long nb = b;
@@ -61,4 +100,30 @@ unsigned long long modularisHatvanyozas(unsigned long long a,unsigned long long 
         }
         na = (na*na)%nm;
     }
+}
+
+unsigned long long modInverse(unsigned long long a, unsigned long long m) {
+    unsigned long long x, y;
+    unsigned long long g = extendedGCD(a, m, &x, &y);
+    
+    if (g != 1) {
+        printf("Inverz nem létezik.\n");
+        return -1; // Nincs inverz
+    } else {
+        return (x % m + m) % m;
+    }
+}
+
+// Kiterjesztett Euklideszi algoritmus
+unsigned long long extendedGCD(unsigned long long a, unsigned long long b, unsigned long long *x, unsigned long long *y) {
+    if (b == 0) {
+        *x = 1;
+        *y = 0;
+        return a;
+    }
+    unsigned long long x1, y1;
+    unsigned long long gcd = extendedGCD(b, a % b, &x1, &y1);
+    *x = y1;
+    *y = x1 - (a / b) * y1;
+    return gcd;
 }
